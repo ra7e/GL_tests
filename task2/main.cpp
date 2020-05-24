26cd723e8a8ea154d571a5f5ce7738849a567002
ra7e <artemnakorenko59@gmail.com>
2020-05-24 14:53:30 +0300

    fix

--- task2/main.cpp
+++ task2/main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main(int argc, char** argv)
 {
   int sum = 0;
-  for(int i = 0; i <= atoi(argv[1]); i++)
-  {
-        sum += i * atoi(argv[2]);
-  }
+  int first = 1;
+
+  sum = (atoi(argv[2]) * atoi(argv[1]) * (1 + atoi(argv[1]))) / 2;
+  // it`s a arithmetic progression - S = n*(A1 + An)/2.
+  // Also we need to consider distance between bank and first span, thats why
+  // we have n+1.
   cout << sum << endl;
 
   return 0;
